f7a30d069596b832d7592623e0dd8b927f6e2d49
yoshinorin <yoshinorin.net@outlook.com>
2021-05-13 23:17:36 +0900

    chore: udpate .env.example & add scripts for env variables

---
+++ scripts/devenv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export QUALTET_DB_DATASOURCE_URL="jdbc:mariadb://127.0.0.1/qualtet?useUnicode=true&characterEncoding=utf8mb4"
+export QUALTET_DB_USER="root"
+export QUALTET_DB_PASSWORD="pass"
+
+export QUALTET_HTTP_BIND_ADDRESS="0.0.0.0"
+export QUALTET_HTTP_PORT=9001
