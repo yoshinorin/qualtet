1fb2420d8e1a3f7b71eb06da8b2efe702adba95d
yoshinorin <yoshinorin.net@outlook.com>
2021-05-27 00:44:58 +0900

    feat: implement RequestContent case class

--- src/main/scala/net/yoshinorin/qualtet/domains/models/contents/Content.scala
+++ src/main/scala/net/yoshinorin/qualtet/domains/models/contents/Content.scala
@@ -2,7 +2,9 @@ package net.yoshinorin.qualtet.domains.models.contents
 
 import java.time.ZonedDateTime
 import java.util.UUID
+import io.circe.Decoder
 import io.circe.Encoder
+import io.circe.generic.semiauto.deriveDecoder
 import io.circe.generic.semiauto.deriveEncoder
 
 final case class Content(
@@ -20,4 +22,41 @@ final case class Content(
 object Content {
   implicit val encodeContent: Encoder[Content] = deriveEncoder[Content]
   implicit val encodeContents: Encoder[List[Content]] = Encoder.encodeList[Content]
+  implicit val decodeContent: Decoder[Content] = deriveDecoder[Content]
+  implicit val decodeContents: Decoder[List[Content]] = Decoder.decodeList[Content]
+}
+
+final case class RequestContent(
+  id: String = UUID.randomUUID().toString,
+  authorId: String,
+  contentTypeId: String,
+  path: String,
+  title: String,
+  rawContent: String,
+  htmlContent: String,
+  publishedAt: Long = ZonedDateTime.now.toEpochSecond,
+  updatedAt: Long = ZonedDateTime.now.toEpochSecond
+)
+
+object RequestContent {
+
+  implicit val encodeRequestContent: Encoder[RequestContent] = deriveEncoder[RequestContent]
+  implicit val encodeRequestContents: Encoder[List[RequestContent]] = Encoder.encodeList[RequestContent]
+  implicit val decodeRequestContent: Decoder[RequestContent] = deriveDecoder[RequestContent]
+  implicit val decodeRequestContents: Decoder[List[RequestContent]] = Decoder.decodeList[RequestContent]
+
+  /* TODO
+  def apply(
+    id: String,
+    authorId: String,
+    contentTypeId: String,
+    path: String,
+    title: String,
+    rawContent: String,
+    htmlContent: String,
+    publishedAt: Long,
+    updatedAt: Long
+  ): RequestContent =
+    new RequestContent(id, authorId, contentTypeId, path, title, rawContent, htmlContent, publishedAt, updatedAt)
+ */
 }
