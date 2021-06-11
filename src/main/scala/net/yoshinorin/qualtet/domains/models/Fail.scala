64ed64f63ea71e995f54f448c833c1461a6f9dc4
yoshinorin <yoshinorin.net@outlook.com>
2021-06-12 02:04:37 +0900

    feat: create Fail case class

---
+++ src/main/scala/net/yoshinorin/qualtet/domains/models/Fail.scala
@@ -0,0 +1,17 @@
+package net.yoshinorin.qualtet.domains.models
+
+import io.circe.Encoder
+import io.circe.generic.semiauto._
+
+sealed trait Fail extends Exception
+
+object Fail {
+
+  implicit val encodeFail: Encoder[Fail] = deriveEncoder[Fail]
+
+  case class NotFound(message: String) extends Fail
+  case class Unauthorized(message: String) extends Fail
+  case class UnprocessableEntity(message: String) extends Fail
+  case object Forbidden extends Fail
+
+}
