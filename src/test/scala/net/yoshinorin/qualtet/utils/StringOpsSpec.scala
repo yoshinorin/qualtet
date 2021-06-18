9e4b6a40ca6260e624b00f4edc97e1a50de63fa0
yoshinorin <yoshinorin.net@outlook.com>
2021-06-19 01:21:52 +0900

    test: add test case for StringOperations

---
+++ src/test/scala/net/yoshinorin/qualtet/utils/StringOpsSpec.scala
@@ -0,0 +1,70 @@
+package net.yoshinorin.qualtet.utils
+
+import net.yoshinorin.qualtet.utils.StringOps.StringOps
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import org.scalatest.wordspec.AnyWordSpec
+
+// testOnly utils.StringOpsSpec
+class StringOpsSpec extends AnyWordSpec {
+
+  import net.yoshinorin.qualtet.utils.StringOps._
+
+  "StringOps - stripHtmlTags" should {
+
+    "replace all html tags" in {
+      val x = "<html>aiu<p>eo</p><script>kakiku</script>keko<a>sashisu</a>seso<br><code></code></html>owari".stripHtmlTags
+      x mustBe "aiueokakikukekosashisusesoowari"
+    }
+
+  }
+
+  "StringOps - stripHtmlSpecialChars" should {
+
+    "replace all html special chars" in {
+      val x = "a&amp;b&#38;c&#169;d&#x27;e&quot;f&#xA0;g&lt;h&gt;i&quot;j".stripHtmlSpecialChars
+      x mustBe "abcdefghij"
+    }
+
+  }
+
+  "StringOps - hasIgnoreChars" should {
+
+    "detected ignore chars" in {
+      "test;test".hasIgnoreChars mustBe true
+      "\\testtest".hasIgnoreChars mustBe true
+      "%testtest".hasIgnoreChars mustBe true
+      "testtest%".hasIgnoreChars mustBe true
+      "test,test".hasIgnoreChars mustBe true
+      "testtest*".hasIgnoreChars mustBe true
+      "testtes*t".hasIgnoreChars mustBe true
+      "test&test".hasIgnoreChars mustBe true
+      "testt@est".hasIgnoreChars mustBe true
+      "te/sttest".hasIgnoreChars mustBe true
+      "tes.ttest".hasIgnoreChars mustBe true
+      "test。test".hasIgnoreChars mustBe true
+      "test、test".hasIgnoreChars mustBe true
+      "test。test".hasIgnoreChars mustBe true
+      "testte、st".hasIgnoreChars mustBe true
+      "testtes＿t".hasIgnoreChars mustBe true
+      "testtes_t".hasIgnoreChars mustBe true
+      "testtes(t".hasIgnoreChars mustBe true
+      "testtes)t".hasIgnoreChars mustBe true
+      "testtesあt".hasIgnoreChars mustBe false
+      "testtesいt".hasIgnoreChars mustBe false
+      "testtest".hasIgnoreChars mustBe false
+    }
+
+  }
+
+  "StringOps - filterIgnoreChars" should {
+
+    "filterd ignore chars" in {
+      "testtesあt".filterIgnoreChars mustBe "testtesあt"
+      "testtesいt".filterIgnoreChars mustBe "testtesいt"
+      "test$%^&@!test".filterIgnoreChars mustBe "testtest"
+      "\\acb。.def///ghi%_jkl*mn**op$qr{}s\\tuあいう。。えお、かきく!!け!こ".filterIgnoreChars mustBe "acbdefghijklmnopqrstuあいうえおかきくけこ"
+    }
+
+  }
+
+}
