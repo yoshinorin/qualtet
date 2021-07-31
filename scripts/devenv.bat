8fc3a6067dd2c9afb7260a6235d30e65b356af67
yoshinorin <yoshinorin.net@outlook.com>
2021-08-01 07:41:11 +0900

    docs: initial commit

--- scripts/devenv.bat
+++
@@ -1,6 +0,0 @@
-setx QUALTET_DB_DATASOURCE_URL "jdbc:mariadb://127.0.0.1/qualtet?useUnicode=true&characterEncoding=utf8mb4"
-setx QUALTET_DB_USER "root"
-setx QUALTET_DB_PASSWORD "pass"
-
-setx QUALTET_HTTP_BIND_ADDRESS "0.0.0.0"
-setx QUALTET_HTTP_PORT 9001
--- src/main/scala/net/yoshinorin/qualtet/domains/models/BaseRequestFormat.scala
+++
@@ -1,7 +0,0 @@
-package net.yoshinorin.qualtet.domains.models
-
-sealed abstract class BaseRequestFormat[T](value: String) {
-
-  protected def validate: Either[Message, T]
-
-}
