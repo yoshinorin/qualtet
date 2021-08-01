72dab68dc2e4d4e2908989f17f073b122dd1a71c
yoshinorin <yoshinorin.net@outlook.com>
2021-08-01 11:00:09 +0900

    test: add test for QueryParameters

---
+++ src/test/scala/net/yoshinorin/qualtet/http/QueryParameterSpec.scala
@@ -0,0 +1,33 @@
+package net.yoshinorin.qualtet.http
+
+import org.scalatest.wordspec.AnyWordSpec
+
+// testOnly net.yoshinorin.qualtet.http.QueryParameterSpec
+class QueryParameterSpec extends AnyWordSpec {
+
+  "QueryParameter" should {
+    "default instance" in {
+      val queryParameter = ArticlesQueryParameter()
+
+      assert(queryParameter.page == 1)
+      assert(queryParameter.limit == 10)
+      assert(queryParameter.offset == 0)
+    }
+
+    /* TODO:
+    "limit is bigger than 10" in {
+      val queryParameter = ArticlesQueryParameter(page = 1, limit = 11)
+
+      assert(queryParameter.limit == 10)
+    }
+
+    "page is bigger than 1" in {
+      val queryParameter = ArticlesQueryParameter(page = 5)
+
+      assert(queryParameter.page == 5)
+      assert(queryParameter.offset == 50)
+    }
+   */
+  }
+
+}
