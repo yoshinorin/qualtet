e6bb61275acdccfc69b7e3819a949c3a6647ab56
yoshinorin <yoshinorin.net@outlook.com>
2022-01-06 23:13:31 +0900

    refactor: DRY

--- src/main/scala/net/yoshinorin/qualtet/utils/Converters.scala
+++ src/main/scala/net/yoshinorin/qualtet/utils/Converters.scala
@@ -4,7 +4,7 @@ import cats.implicits._
 
 object Converters {
 
-  def zipFromSeparatedComma[A](k: Option[String], v: Option[String])(f: (String, String) => A): Option[List[A]] = {
+  private def getKeyValueOrNone(k: Option[String], v: Option[String]): Option[(List[String], List[String])] = {
     val keys = k match {
       case None => return None
       case Some(x) => x.split(",").map(_.trim).toList
@@ -16,22 +16,21 @@ object Converters {
     if (keys.size =!= values.size) {
       return None
     }
-    Option(keys.zip(values).map(x => f(x._1, x._2)))
+    Option(keys, values)
+  }
+
+  def zipFromSeparatedComma[A](k: Option[String], v: Option[String])(f: (String, String) => A): Option[List[A]] = {
+    this.getKeyValueOrNone(k, v) match {
+      case None => None
+      case Some(kv) => Option(kv._1.zip(kv._2).map(x => f(x._1, x._2)))
+    }
   }
 
   def zipWithGroupByFromSeparatedComma[A](k: Option[String], v: Option[String])(f: (String, List[(String, String)]) => A): Option[List[A]] = {
-    val keys = k match {
-      case None => return None
-      case Some(x) => x.split(",").map(_.trim).toList
+    this.getKeyValueOrNone(k, v) match {
+      case None => None
+      case Some(kv) => Option(kv._1.zip(kv._2).groupBy(_._1).map(x => f(x._1, x._2)).toList)
     }
-    val values = v match {
-      case None => return None
-      case Some(x) => x.split(",").map(_.trim).toList
-    }
-    if (keys.size =!= values.size) {
-      return None
-    }
-    Option(keys.zip(values).groupBy(_._1).map(x => f(x._1, x._2)).toList)
   }
 
 }
