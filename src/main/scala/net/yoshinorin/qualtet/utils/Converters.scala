71e2b5c7f0f2656851335d6375702716f3975f2f
yoshinorin <yoshinorin.net@outlook.com>
2022-01-07 20:31:04 +0900

    refactor: rename function

--- src/main/scala/net/yoshinorin/qualtet/utils/Converters.scala
+++ src/main/scala/net/yoshinorin/qualtet/utils/Converters.scala
@@ -4,7 +4,7 @@ import cats.implicits._
 
 object Converters {
 
-  private def getKeyValueOrNone(k: Option[String], v: Option[String]): Option[(List[String], List[String])] = {
+  private def getKeyValues(k: Option[String], v: Option[String]): Option[(List[String], List[String])] = {
     val keys = k match {
       case None => return None
       case Some(x) => x.split(",").map(_.trim).toList
@@ -23,14 +23,14 @@ object Converters {
 
     // equally: def zip[A](k: Option[String], v: Option[String])(f: (String, String) => A)
     def zip[A](f: (String, String) => A): Option[List[A]] = {
-      getKeyValueOrNone(kv._1, kv._2) match {
+      getKeyValues(kv._1, kv._2) match {
         case None => None
         case Some(kv) => Option(kv._1.zip(kv._2).map(x => f(x._1, x._2)))
       }
     }
 
     def zipWithGroupBy[A](f: (String, List[(String, String)]) => A): Option[List[A]] = {
-      getKeyValueOrNone(kv._1, kv._2) match {
+      getKeyValues(kv._1, kv._2) match {
         case None => None
         case Some(kv) => Option(kv._1.zip(kv._2).groupBy(_._1).map(x => f(x._1, x._2)).toList)
       }
