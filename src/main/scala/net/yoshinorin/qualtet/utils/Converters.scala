909a6ac56977a2f879dcc77db71b8343640bec19
yoshinorin <yoshinorin.net@outlook.com>
2022-01-08 22:11:04 +0900

    refactor: conditional operator to pattern match

--- src/main/scala/net/yoshinorin/qualtet/utils/Converters.scala
+++ src/main/scala/net/yoshinorin/qualtet/utils/Converters.scala
@@ -4,33 +4,32 @@ import cats.implicits._
 
 object Converters {
 
-  private def getKeyValues(k: Option[String], v: Option[String]): Option[(List[String], List[String])] = {
-    val keys = k match {
-      case None => return None
-      case Some(x) => x.split(",").map(_.trim).toList
+  private def getKeyValues(kv: (Option[String], Option[String])): Option[(List[String], List[String])] = {
+    kv match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(k), Some(v)) =>
+        val (x, y) = (k.split(",").map(_.trim).toList, v.split(",").map(_.trim).toList)
+        if (x.size =!= y.size) {
+          None
+        } else {
+          Option(x, y)
+        }
     }
-    val values = v match {
-      case None => return None
-      case Some(x) => x.split(",").map(_.trim).toList
-    }
-    if (keys.size =!= values.size) {
-      return None
-    }
-    Option(keys, values)
   }
 
   implicit class KeyValueCommaSeparatedString(kv: (Option[String], Option[String])) {
 
     // equally: def zip[A](k: Option[String], v: Option[String])(f: (String, String) => A)
     def zip[A](f: (String, String) => A): Option[List[A]] = {
-      getKeyValues(kv._1, kv._2) match {
+      getKeyValues(kv) match {
         case None => None
         case Some(kv) => Option(kv._1.zip(kv._2).map(x => f(x._1, x._2)))
       }
     }
 
     def zipWithGroupBy[A](f: (String, List[(String, String)]) => A): Option[List[A]] = {
-      getKeyValues(kv._1, kv._2) match {
+      getKeyValues(kv) match {
         case None => None
         case Some(kv) => Option(kv._1.zip(kv._2).groupBy(_._1).map(x => f(x._1, x._2)).toList)
       }
