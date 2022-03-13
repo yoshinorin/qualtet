a1877dff080f9cb87b92738a255df4f65b866338
yoshinorin <yoshinorin.net@outlook.com>
2022-03-13 21:58:06 +0900

    test: add ContentTypeRoute spec

---
+++ src/test/scala/net/yoshinorin/qualtet/http/routes/ContentTypeRouteSpec.scala
@@ -0,0 +1,38 @@
+package net.yoshinorin.qualtet.http.routes
+
+import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import net.yoshinorin.qualtet.fixture.Fixture.contentTypeRoute
+import org.scalatest.wordspec.AnyWordSpec
+
+// testOnly net.yoshinorin.qualtet.http.routes.ContentTypeRouteSpec
+class ContentTypeRouteSpec extends AnyWordSpec with ScalatestRouteTest {
+
+  "ContentTypeRoute" should {
+
+    "be return content-types" in {
+      Get("/content-types/") ~> contentTypeRoute.route ~> check {
+        assert(status == StatusCodes.OK)
+        assert(contentType == ContentTypes.`application/json`)
+        // TODO: assert response contents count
+      }
+    }
+
+    "be return content-type:articles" in {
+      Get("/content-types/article") ~> contentTypeRoute.route ~> check {
+        assert(status == StatusCodes.OK)
+        assert(contentType == ContentTypes.`application/json`)
+        assert(responseAs[String].replaceAll("\n", "").replaceAll(" ", "").contains("article"))
+      }
+    }
+
+    "be return content-type:page" in {
+      Get("/content-types/page") ~> contentTypeRoute.route ~> check {
+        assert(status == StatusCodes.OK)
+        assert(contentType == ContentTypes.`application/json`)
+        assert(responseAs[String].replaceAll("\n", "").replaceAll(" ", "").contains("page"))
+      }
+    }
+  }
+
+}
