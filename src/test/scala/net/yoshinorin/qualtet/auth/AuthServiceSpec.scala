28fe1763f1e8663a02e746c219f004173cdd1af5
yoshinorin <yoshinorin.net@outlook.com>
2022-03-15 20:03:22 +0900

    test: use DB instead of mock at AuthServiceSpec

--- src/test/scala/net/yoshinorin/qualtet/auth/AuthServiceSpec.scala
+++ src/test/scala/net/yoshinorin/qualtet/auth/AuthServiceSpec.scala
@@ -1,55 +1,32 @@
 package net.yoshinorin.qualtet.auth
 
-import cats.effect.IO
-import net.yoshinorin.qualtet.domains.services.AuthorService
 import net.yoshinorin.qualtet.domains.models.Fail.{NotFound, Unauthorized}
+import net.yoshinorin.qualtet.domains.models.authors.ResponseAuthor
 import net.yoshinorin.qualtet.fixture.Fixture._
 import net.yoshinorin.qualtet.infrastructure.db.doobie.DoobieContext
-import org.mockito.Mockito
-import org.mockito.Mockito._
 import org.scalatest.wordspec.AnyWordSpec
-import pdi.jwt.JwtAlgorithm
-
-import java.security.SecureRandom
 
 // testOnly net.yoshinorin.qualtet.auth.AuthServiceSpec
 class AuthServiceSpec extends AnyWordSpec {
 
   implicit val doobieContext: DoobieContext = new DoobieContext()
 
-  val keyPair = new KeyPair("RSA", 2048, SecureRandom.getInstanceStrong)
-  val message: Array[Byte] = SecureRandom.getInstanceStrong.toString.getBytes
-  val signature = new net.yoshinorin.qualtet.auth.Signature("SHA256withRSA", message, keyPair)
-  val jwtInstance = new Jwt(JwtAlgorithm.RS256, keyPair, signature)
-
-  val mockAuthorService: AuthorService = Mockito.mock(classOf[AuthorService])
+  val a: ResponseAuthor = authorService.findByName(author.name).unsafeRunSync().get
 
   "AuthService" should {
 
-    "generate token" in {
-      when(mockAuthorService.findByIdWithPassword(authorId))
-        .thenReturn(IO(Some(author)))
-
-      val authService = new AuthService(mockAuthorService, jwtInstance)
-      val token = authService.generateToken(RequestToken(authorId, "pass")).unsafeRunSync().token
+    "be generate token" in {
+      val token = authService.generateToken(RequestToken(a.id, "pass")).unsafeRunSync().token
       assert(jwtInstance.decode(token).isRight)
     }
 
-    "password is wrong" in {
-      when(mockAuthorService.findByIdWithPassword(authorId))
-        .thenReturn(IO(Some(author)))
-
-      val authService = new AuthService(mockAuthorService, jwtInstance)
+    "be throw exception caused by password is wrong" in {
       assertThrows[Unauthorized] {
-        authService.generateToken(RequestToken(authorId, "wrongPassword")).unsafeRunSync()
+        authService.generateToken(RequestToken(a.id, "wrongPassword")).unsafeRunSync()
       }
     }
 
-    "authorName not found" in {
-      when(mockAuthorService.findByIdWithPassword(authorId2))
-        .thenReturn(IO(None))
-
-      val authService = new AuthService(mockAuthorService, jwtInstance)
+    "be throw exception caused by authorName not found" in {
       assertThrows[NotFound] {
         authService.generateToken(RequestToken(authorId2, "password")).unsafeRunSync()
       }
