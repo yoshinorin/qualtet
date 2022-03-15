8476920ce6bd65e31acdd8fababda6a19a771113
yoshinorin <yoshinorin.net@outlook.com>
2022-03-15 20:24:30 +0900

    test: add test case to AuthServiceSpec

--- src/test/scala/net/yoshinorin/qualtet/auth/AuthServiceSpec.scala
+++ src/test/scala/net/yoshinorin/qualtet/auth/AuthServiceSpec.scala
@@ -12,12 +12,31 @@ class AuthServiceSpec extends AnyWordSpec {
   implicit val doobieContext: DoobieContext = new DoobieContext()
 
   val a: ResponseAuthor = authorService.findByName(author.name).unsafeRunSync().get
+  val a2: ResponseAuthor = authorService.findByName(author2.name).unsafeRunSync().get
 
   "AuthService" should {
 
     "be generate token" in {
       val token = authService.generateToken(RequestToken(a.id, "pass")).unsafeRunSync().token
       assert(jwtInstance.decode(token).isRight)
+    }
+
+    "be find an author from JWT string" in {
+      val token = authService.generateToken(RequestToken(a.id, "pass")).unsafeRunSync().token
+      val author = authService.findAuthorFromJwtString(token).unsafeRunSync().get
+      assert(author.id.value == a.id.value)
+    }
+
+    "be throw exception if JWT is expired" in {
+      assertThrows[Unauthorized] {
+        authService.findAuthorFromJwtString(expiredToken).unsafeRunSync()
+      }
+    }
+
+    "be throw exception if JWT's author not found" in {
+      assertThrows[Unauthorized] {
+        authService.findAuthorFromJwtString(nonExistsUserToken).unsafeRunSync()
+      }
     }
 
     "be throw exception caused by password is wrong" in {
