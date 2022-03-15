41a3f082f47775d94e4dbb18c6be6a3f09871ad9
yoshinorin <yoshinorin.net@outlook.com>
2022-03-15 20:53:49 +0900

    test: use fixture instead of instance in the test spec class

--- src/test/scala/net/yoshinorin/qualtet/auth/JwtSpec.scala
+++ src/test/scala/net/yoshinorin/qualtet/auth/JwtSpec.scala
@@ -1,25 +1,19 @@
 package net.yoshinorin.qualtet.auth
 
 import net.yoshinorin.qualtet.config.Config
+import net.yoshinorin.qualtet.domains.models.Fail.Unauthorized
 import net.yoshinorin.qualtet.domains.models.authors.{Author, AuthorDisplayName, AuthorId, AuthorName}
-import net.yoshinorin.qualtet.fixture.Fixture.validBCryptPassword
+import net.yoshinorin.qualtet.fixture.Fixture.{expiredToken, jwtInstance, validBCryptPassword}
 import org.scalatest.wordspec.AnyWordSpec
-import pdi.jwt.JwtAlgorithm
 import pdi.jwt.exceptions.JwtValidationException
 
-import java.security.SecureRandom
 import wvlet.airframe.ulid.ULID
 
 // testOnly net.yoshinorin.qualtet.auth.JwtSpec
 class JwtSpec extends AnyWordSpec {
 
   "Jwt" should {
-    val keyPair = new KeyPair("RSA", 2048, SecureRandom.getInstanceStrong)
-    val message = SecureRandom.getInstanceStrong.toString.getBytes
-    val signature = new net.yoshinorin.qualtet.auth.Signature("SHA256withRSA", message, keyPair)
-
-    "encode and decode" in {
-      val jwtInstance = new Jwt(JwtAlgorithm.RS256, keyPair, signature)
+    "be encode and decode" in {
       val id = ULID.newULIDString.toLowerCase
       val jwtString = jwtInstance.encode(
         Author(
@@ -43,8 +37,7 @@ class JwtSpec extends AnyWordSpec {
 
     }
 
-    "not signed JSON" in {
-      val jwtInstance = new Jwt(JwtAlgorithm.RS256, keyPair, signature)
+    "be throw exception caused by not signed JSON" in {
       val maybeJwtClaims = jwtInstance.decode(
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
       )
