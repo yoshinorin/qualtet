e61342d5d836d10a4f09b216e6501fe8f3388a50
yoshinorin <yoshinorin.net@outlook.com>
2022-05-04 22:06:28 +0900

    refactor: delete unnecessary function

--- src/main/scala/net/yoshinorin/qualtet/domains/sitemaps/DoobieSitemapsRepository.scala
+++ src/main/scala/net/yoshinorin/qualtet/domains/sitemaps/DoobieSitemapsRepository.scala
@@ -5,10 +5,6 @@ import RepositoryRequests.Get
 
 class DoobieSitemapsRepository extends SitemapsRepository {
 
-  def get: ConnectionIO[Seq[Url]] = {
-    DoobieSitemapsQuery.get.to[Seq]
-  }
-
   def dispatch(requests: Get): ConnectionIO[Seq[Url]] = {
     DoobieSitemapsQuery.get.to[Seq]
   }
