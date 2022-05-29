315b37390ee05d287256a39a71eb27f1ff9163ef
yoshinorin <yoshinorin.net@outlook.com>
2022-05-29 12:15:21 +0900

    refactor: remove implicit parameter from `ServiceLogic` and add `final` to case class

--- src/main/scala/net/yoshinorin/qualtet/domains/ServiceLogic.scala
+++ src/main/scala/net/yoshinorin/qualtet/domains/ServiceLogic.scala
@@ -8,19 +8,19 @@ import net.yoshinorin.qualtet.domains.repository.Repository
 import net.yoshinorin.qualtet.infrastructure.db.doobie.DoobieContextBase
 
 sealed trait ServiceLogic[R]
-case class Continue[T, R](request: RepositoryRequest[T], next: T => ServiceLogic[R]) extends ServiceLogic[R]
-case class Done[R](value: R) extends ServiceLogic[R]
+final case class Continue[T, R](request: RepositoryRequest[T], next: T => ServiceLogic[R]) extends ServiceLogic[R]
+final case class Done[R](value: R) extends ServiceLogic[R]
 
 object ServiceLogic {
 
   // with transaction
-  def runWithTransaction[R](serviceLogic: ServiceLogic[R])(implicit doobieContext: DoobieContextBase): IO[R] = serviceLogic match {
-    case continue: Continue[_, R] => runContinueWithTransaction(continue)
+  def runWithTransaction[R](serviceLogic: ServiceLogic[R])(doobieContext: DoobieContextBase): IO[R] = serviceLogic match {
+    case continue: Continue[_, R] => runContinueWithTransaction(continue)(doobieContext)
     case Done(value) => IO(value)
   }
 
-  private def runContinueWithTransaction[T, R](continue: Continue[T, R])(implicit doobieContext: DoobieContextBase): IO[R] = {
-    Repository.dispatch(continue.request).transact(doobieContext.transactor).flatMap { t => runWithTransaction(continue.next(t)) }
+  private def runContinueWithTransaction[T, R](continue: Continue[T, R])(doobieContext: DoobieContextBase): IO[R] = {
+    Repository.dispatch(continue.request).transact(doobieContext.transactor).flatMap { t => runWithTransaction(continue.next(t))(doobieContext) }
   }
 
   // without transaction
