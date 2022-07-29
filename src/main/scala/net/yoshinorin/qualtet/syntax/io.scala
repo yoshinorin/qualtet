11db7ec4abc53d6e1f2cc8deb787d21151189901
yoshinorin <yoshinorin.net@outlook.com>
2022-07-29 23:15:12 +0900

    fix: add `ClassTag` to `io syntax`

--- src/main/scala/net/yoshinorin/qualtet/syntax/io.scala
+++ src/main/scala/net/yoshinorin/qualtet/syntax/io.scala
@@ -1,14 +1,15 @@
 package net.yoshinorin.qualtet.syntax
 
 import cats.effect.IO
+import scala.reflect.ClassTag
 
 trait io {
 
-  implicit final class IOOps[A](val io: IO[Option[A]]) {
+  implicit final class IOOps[A : ClassTag](val io: IO[Option[A]]) {
     def throwIfNone[F <: Throwable](t: F): IO[A] = {
       io.flatMap {
-        case None => IO.raiseError(t)
         case Some(a: A) => IO(a)
+        case _ => IO.raiseError(t)
       }
     }
   }
