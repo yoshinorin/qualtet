bc1c25ac233f8089a6b46e3707755e49640019e1
yoshinorin <yoshinorin.net@outlook.com>
2022-08-03 00:32:19 +0900

    refactor: delete unnecessary implicit

--- src/main/scala/net/yoshinorin/qualtet/message/Fail.scala
+++ src/main/scala/net/yoshinorin/qualtet/message/Fail.scala
@@ -1,13 +1,8 @@
 package net.yoshinorin.qualtet.message
 
-import io.circe.Encoder
-import io.circe.generic.semiauto._
-
 sealed trait Fail extends Exception
 
 object Fail {
-
-  implicit val encodeFail: Encoder[Fail] = deriveEncoder[Fail]
 
   final case class NotFound(message: String) extends Fail
   final case class Unauthorized(message: String = "Unauthorized") extends Fail
