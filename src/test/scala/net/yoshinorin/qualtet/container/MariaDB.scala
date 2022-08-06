125a982c2fd88d4c48951eed94f0850c41de6474
yoshinorin <yoshinorin.net@outlook.com>
2022-08-06 17:05:50 +0900

    chore: comment out unused test package

--- src/test/scala/net/yoshinorin/qualtet/container/MariaDB.scala
+++ src/test/scala/net/yoshinorin/qualtet/container/MariaDB.scala
@@ -1,4 +1,4 @@
-package net.yoshinorin.qualtet.container
+// package net.yoshinorin.qualtet.container
 
 /* NOTE:
 This object is debris of using testcontainers (I can't implement & use it for testing)
