5e2a8b2c6e7541161e2c250ab6d9164e3bfc7142
yoshinorin <yoshinorin.net@outlook.com>
2022-08-11 01:14:06 +0900

    refactor: add `ClassTag` to `eitherT syntax`

--- src/main/scala/net/yoshinorin/qualtet/syntax/eitherT.scala
+++ src/main/scala/net/yoshinorin/qualtet/syntax/eitherT.scala
@@ -2,13 +2,14 @@ package net.yoshinorin.qualtet.syntax
 
 import cats.data.EitherT
 import cats.effect.IO
+import scala.reflect.ClassTag
 
 trait eitherT {
 
-  implicit final class ValidationOps[A, F <: Throwable](v: EitherT[IO, F, A]) {
+  implicit final class ValidationOps[A: ClassTag, F <: Throwable](v: EitherT[IO, F, A]) {
     def andThrow: IO[A] = {
       v.value.flatMap {
-        case Right(v: A) => IO(v)
+        case Right(v) => IO(v)
         case Left(t: Throwable) => IO.raiseError(t)
       }
     }
