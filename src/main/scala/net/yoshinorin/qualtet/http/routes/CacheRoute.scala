5395e21d0eb044e3177116f2b3b7cf0c1d041f83
yoshinorin <yoshinorin.net@outlook.com>
2022-11-03 23:59:31 +0900

    update

--- src/main/scala/net/yoshinorin/qualtet/http/routes/CacheRoute.scala
+++ src/main/scala/net/yoshinorin/qualtet/http/routes/CacheRoute.scala
@@ -1,14 +1,17 @@
 package net.yoshinorin.qualtet.http.routes
 
 import cats.effect.IO
-import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import cats.data.Kleisli
+import cats.data.OptionT
+import org.http4s.HttpRoutes
+import org.http4s.server._
+import org.http4s._
+import net.yoshinorin.qualtet.domains.authors.ResponseAuthor
+import org.http4s.headers.Authorization
+import org.http4s.dsl.io._
 import net.yoshinorin.qualtet.cache.CacheService
 import net.yoshinorin.qualtet.auth.AuthService
 import net.yoshinorin.qualtet.http.{Authentication, ResponseHandler}
-
-import cats.effect.unsafe.implicits.global
 
 class CacheRoute(
   authService: AuthService,
@@ -16,24 +19,36 @@ class CacheRoute(
 ) extends Authentication(authService)
     with ResponseHandler {
 
-  def route: Route = {
-    pathPrefix("caches") {
-      pathEndOrSingleSlash {
-        delete {
-          authenticate { _ =>
-            onSuccess(
-              cacheService
-                .invalidateAll()
-                .handleErrorWith { e => IO.pure(e) }
-                .unsafeToFuture()
-            ) {
-              case e: Exception =>
-                httpResponse(e)
-              case _ => httpResponse(NoContent)
-            }
-          }
-        }
-      }
+  val onFailure: AuthedRoutes[String, IO] =
+    Kleisli(req => OptionT.liftF(Forbidden(req.context)))
+
+  val authMiddleware = AuthMiddleware(authUserHeader, onFailure)
+
+  val authUserHeader: Kleisli[IO, Request[IO], Either[String, ResponseAuthor]] = Kleisli({ request =>
+    val h = request.headers.get[Authorization]
+    for {
+      author <- authService.findAuthorFromJwtString(h.toString())
+    } yield author match {
+      case None => Left("Unauthorized")
+      case Some(a) => Right(a)
     }
-  }
+  })
+
+  def route: HttpRoutes[IO] = authMiddleware(authedRoutes)
+
+  val authedRoutes: AuthedRoutes[ResponseAuthor, IO] =
+    AuthedRoutes.of {
+      // TODO: DRY
+      case DELETE -> Root / "caches" as author =>
+        for {
+          _ <- cacheService.invalidateAll()
+          response <- NoContent()
+        } yield response
+      case DELETE -> Root / "caches" / "" as author =>
+        for {
+          _ <- cacheService.invalidateAll()
+          response <- NoContent()
+        } yield response
+    }
+
 }
