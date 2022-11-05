824d4d737c3d2a7188e6064aeb22eb40ed8344c0
yoshinorin <yoshinorin.net@outlook.com>
2022-11-06 01:27:12 +0900

    update

--- src/main/scala/net/yoshinorin/qualtet/http/routes/AuthRoute.scala
+++ src/main/scala/net/yoshinorin/qualtet/http/routes/AuthRoute.scala
@@ -5,6 +5,7 @@ import org.http4s.HttpRoutes
 import org.http4s.headers.`Content-Type`
 import org.http4s._
 import org.http4s.dsl.io._
+import net.yoshinorin.qualtet.message.Fail
 import net.yoshinorin.qualtet.auth.{AuthService, RequestToken}
 import net.yoshinorin.qualtet.http.{RequestDecoder, ResponseHandler}
 import net.yoshinorin.qualtet.syntax._
@@ -29,6 +30,18 @@ class AuthRoute(authService: AuthService) extends RequestDecoder with ResponseHa
             }
         }
       }
+    }.handleErrorWith {
+      // TODO: DRY & Logging
+      case f: Fail => f match {
+        case Fail.NotFound(message) => NotFound(message)
+        // TODO: `forbidden` to `unauthorized`
+        case Fail.Unauthorized(message) => Forbidden(message)
+        case Fail.UnprocessableEntity(message) => UnprocessableEntity(message)
+        case Fail.BadRequest(message) => BadRequest(message)
+        case Fail.Forbidden(message) => Forbidden(message)
+        case Fail.InternalServerError(message) => InternalServerError(message)
+      }
+      case _ => InternalServerError("Internal Server Error")
     }
   }
 
