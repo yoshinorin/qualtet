4742851db68a6be9d6bc328dc9af6a4573f85d12
yoshinorin <yoshinorin.net@outlook.com>
2022-11-29 23:10:55 +0900

    update

--- src/main/scala/net/yoshinorin/qualtet/http/routes/CacheRoute.scala
+++ src/main/scala/net/yoshinorin/qualtet/http/routes/CacheRoute.scala
@@ -1,6 +1,7 @@
 package net.yoshinorin.qualtet.http.routes
 
 import cats.effect.IO
+import cats.implicits._
 import cats.data.Kleisli
 import cats.data.OptionT
 import org.http4s.HttpRoutes
@@ -11,6 +12,7 @@ import org.http4s.headers.Authorization
 import org.http4s.dsl.io._
 import net.yoshinorin.qualtet.cache.CacheService
 import net.yoshinorin.qualtet.auth.AuthService
+import org.http4s.dsl.impl.Auth
 
 class CacheRoute(
   authService: AuthService,
@@ -28,23 +30,23 @@ class CacheRoute(
   }
   */
 
+  // とりあえず動く（Unautorizedにはなる）コード
+  val authUserHeader: Kleisli[IO, Request[IO], Either[String, ResponseAuthor]] =
+    Kleisli({ request =>
+      val c = for {
+        t <- request.headers.get[Authorization].toRight("todo1")
+      } yield t
+      for {
+        // TODO: ここからBearerだけ抜き出す
+        a <- authService.findAuthorFromJwtString(c.toString())
+      } yield a match {
+        case None => Left("todo2")
+        case Some(v) => Right(v)
+      }
+    })
 
-  val onFailure: AuthedRoutes[String, IO] =
-    Kleisli(req => OptionT.liftF(Forbidden(req.context)))
-
-  val authMiddleware = AuthMiddleware(authUserHeader, onFailure)
-
-  val authUserHeader: Kleisli[IO, Request[IO], Either[String, ResponseAuthor]] = Kleisli({ request =>
-    val h = request.headers.get[Authorization]
-    // https://http4s.org/v1/docs/auth.html#authorization-header
-    for {
-      author <- authService.findAuthorFromJwtString(h.toString())
-    } yield author match {
-      case None => Left("Unauthorized")
-      case Some(a) => Right(a)
-    }
-  })
-
+  val onFailure: AuthedRoutes[String, IO] = Kleisli(req => OptionT.liftF(Forbidden(req.context)))
+  val authMiddleware: AuthMiddleware[IO, ResponseAuthor] = AuthMiddleware(authUserHeader, onFailure)
   def route: HttpRoutes[IO] = authMiddleware(authedRoutes)
   // caches
   val authedRoutes: AuthedRoutes[ResponseAuthor, IO] =
