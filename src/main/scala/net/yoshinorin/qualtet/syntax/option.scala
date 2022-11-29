5dd0790163cb245c686fa343d60f871cb493a101
yoshinorin <yoshinorin.net@outlook.com>
2022-11-30 00:22:49 +0900

    feat(syntax): implement `orThrow` for `Option`

--- src/main/scala/net/yoshinorin/qualtet/syntax/option.scala
+++ src/main/scala/net/yoshinorin/qualtet/syntax/option.scala
@@ -5,6 +5,11 @@ trait option {
   implicit final class OptionOps[T](val s: Option[T]) {
     @SuppressWarnings(Array("org.wartremover.warts.ToString"))
     def stringify: String = s.getOrElse("").toString()
+
+    def orThrow(t: Throwable): T = s match {
+      case None => throw t
+      case Some(t) => t
+    }
   }
 
 }
