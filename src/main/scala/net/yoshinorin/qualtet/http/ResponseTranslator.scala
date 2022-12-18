2350ae627f6da7fdb4edbf66b2c0ada166e05248
yoshinorin <yoshinorin.net@outlook.com>
2022-12-18 21:06:04 +0900

    feat(http/error): return as JSON format when domains raises exception

--- src/main/scala/net/yoshinorin/qualtet/http/ResponseTranslator.scala
+++ src/main/scala/net/yoshinorin/qualtet/http/ResponseTranslator.scala
@@ -2,9 +2,10 @@ package net.yoshinorin.qualtet.http
 
 import cats.effect.IO
 import org.http4s.dsl.io._
+import org.http4s.MediaType
 import org.http4s.Response
 import org.http4s.Challenge
-import org.http4s.headers.`WWW-Authenticate`
+import org.http4s.headers.{`Content-Type`,`WWW-Authenticate`}
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import net.yoshinorin.qualtet.message.Fail
 import net.yoshinorin.qualtet.message.Message
@@ -17,12 +18,12 @@ object ResponseTranslator {
 
   private[this] def failToResponse(f: Fail): IO[Response[IO]] = {
     f match {
-      case Fail.NotFound(message) => NotFound(message)
+      case Fail.NotFound(message) => NotFound(Message(message).asJson, `Content-Type`(MediaType.application.json))
       case Fail.Unauthorized(message) => Unauthorized(`WWW-Authenticate`(Challenge("Bearer", "Unauthorized")))
-      case Fail.UnprocessableEntity(message) => UnprocessableEntity(message)
-      case Fail.BadRequest(message) => BadRequest(message)
-      case Fail.Forbidden(message) => Forbidden(message)
-      case Fail.InternalServerError(message) => InternalServerError(message)
+      case Fail.UnprocessableEntity(message) => UnprocessableEntity(Message(message).asJson, `Content-Type`(MediaType.application.json))
+      case Fail.BadRequest(message) => BadRequest(Message(message).asJson, `Content-Type`(MediaType.application.json))
+      case Fail.Forbidden(message) => Forbidden(Message(message).asJson, `Content-Type`(MediaType.application.json))
+      case Fail.InternalServerError(message) => InternalServerError(Message(message).asJson, `Content-Type`(MediaType.application.json))
     }
   }
 
