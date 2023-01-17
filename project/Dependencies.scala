221642c3ace35681765cef5cc8eb021d9efbef7c
yoshinorin <yoshinorin.net@outlook.com>
2023-01-17 22:37:24 +0900

    chore(deps): update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val jsoniterVersion = "2.20.1"
+  val jsoniterVersion = "2.20.3"
   val doobieVersion = "1.0.0-RC2"
   val jwtScalaVersion = "9.1.2"
   val flywayVersion = "9.11.0"
@@ -11,7 +11,7 @@ object Dependencies {
   val dependencies = Seq(
     "com.typesafe" % "config" % "1.4.2",
     // NOTE: doobie 1.0.0-RC2 dependes on cats-effect 3.3.4, but cats-effect keep fully compatibility with 3.x
-    "org.typelevel" %% "cats-effect" % "3.4.4",
+    "org.typelevel" %% "cats-effect" % "3.4.5",
     "org.http4s" %% "http4s-dsl" % http4sVersion,
     "org.http4s" %% "http4s-ember-server" % http4sVersion,
     "org.http4s" %% "http4s-ember-client" % http4sVersion % Test,
@@ -19,10 +19,10 @@ object Dependencies {
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % jsoniterVersion,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "compile-internal",
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "test-internal",
-    "org.mariadb.jdbc" % "mariadb-java-client" % "3.1.0",
+    "org.mariadb.jdbc" % "mariadb-java-client" % "3.1.1",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "org.wvlet.airframe" %% "airframe-ulid" % "22.12.6",
+    "org.wvlet.airframe" %% "airframe-ulid" % "23.1.1",
     "com.github.ben-manes.caffeine" % "caffeine" % "3.1.2",
     "org.flywaydb" % "flyway-core" % flywayVersion,
     "org.flywaydb" % "flyway-mysql" % flywayVersion,
@@ -31,7 +31,7 @@ object Dependencies {
     "org.codehaus.janino" % "janino" % "3.1.9",
     "org.springframework.security" % "spring-security-core" % "6.0.1",
     "org.slf4j" % "slf4j-api" % "2.0.6",
-    "org.scalatest" %% "scalatest" % "3.2.14" % "test",
+    "org.scalatest" %% "scalatest" % "3.2.15" % "test",
     "org.mockito" % "mockito-core" % "4.11.0" % "test"
   )
 }
