e7dad5eb2aecef5905a8f701770e28e3df5fe1ff
yoshinorin <yoshinorin.net@outlook.com>
2023-02-15 23:29:16 +0900

    fix(contents): return contentId when `POST` a content

--- src/main/scala/net/yoshinorin/qualtet/http/routes/ContentRoute.scala
+++ src/main/scala/net/yoshinorin/qualtet/http/routes/ContentRoute.scala
@@ -28,8 +28,8 @@ class ContentRoute[F[_]: Monad](
       c match {
         case Left(f) => throw f
         case Right(c) =>
-          contentService.createContentFromRequest(payload._1.name, c).flatMap { _ =>
-            Created(c.asJson, `Content-Type`(MediaType.application.json))
+          contentService.createContentFromRequest(payload._1.name, c).flatMap { createdContent =>
+            Created(createdContent.asJson, `Content-Type`(MediaType.application.json))
           }
       }
     }.handleErrorWith(_.logWithStackTrace.andResponse)
