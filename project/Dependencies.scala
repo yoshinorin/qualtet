6539980872fefecfaf60f9f0be53242168608a40
yoshinorin <yoshinorin.net@outlook.com>
2023-02-24 00:40:59 +0900

    chore(deps): update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object Dependencies {
 
-  val jsoniterVersion = "2.20.7"
+  val jsoniterVersion = "2.21.1"
   val doobieVersion = "1.0.0-RC2"
   val jwtScalaVersion = "9.2.0"
-  val flywayVersion = "9.15.0"
+  val flywayVersion = "9.15.1"
   val http4sVersion = "1.0.0-M39"
 
   val dependencies = Seq(
     "com.typesafe" % "config" % "1.4.2",
     // NOTE: doobie 1.0.0-RC2 dependes on cats-effect 3.3.4, but cats-effect keep fully compatibility with 3.x
-    "org.typelevel" %% "cats-effect" % "3.4.7",
+    "org.typelevel" %% "cats-effect" % "3.4.8",
     "org.http4s" %% "http4s-dsl" % http4sVersion,
     "org.http4s" %% "http4s-ember-server" % http4sVersion,
     "org.http4s" %% "http4s-ember-client" % http4sVersion % Test,
@@ -22,14 +22,14 @@ object Dependencies {
     "org.mariadb.jdbc" % "mariadb-java-client" % "3.1.2",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "org.wvlet.airframe" %% "airframe-ulid" % "23.2.4",
-    "com.github.ben-manes.caffeine" % "caffeine" % "3.1.3",
+    "org.wvlet.airframe" %% "airframe-ulid" % "23.2.5",
+    "com.github.ben-manes.caffeine" % "caffeine" % "3.1.4",
     "org.flywaydb" % "flyway-core" % flywayVersion,
     "org.flywaydb" % "flyway-mysql" % flywayVersion,
     "ch.qos.logback" % "logback-classic" % "1.4.5",
-    "net.logstash.logback" % "logstash-logback-encoder" % "7.2",
+    "net.logstash.logback" % "logstash-logback-encoder" % "7.3",
     "org.codehaus.janino" % "janino" % "3.1.9",
-    "org.springframework.security" % "spring-security-core" % "6.0.1",
+    "org.springframework.security" % "spring-security-core" % "6.0.2",
     "org.slf4j" % "slf4j-api" % "2.0.6",
     "org.scalatest" %% "scalatest" % "3.2.15" % "test",
     "org.mockito" % "mockito-core" % "5.1.1" % "test"
