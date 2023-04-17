f3f86b5129e2e1e9ddca4097458800ebd70f46d0
yoshinorin <yoshinorin.net@outlook.com>
2023-04-17 22:44:07 +0900

    chore(deps): update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val jsoniterVersion = "2.22.1"
+  val jsoniterVersion = "2.22.2"
   val doobieVersion = "1.0.0-RC2"
   val jwtScalaVersion = "9.2.0"
   val flywayVersion = "9.16.3"
@@ -11,7 +11,7 @@ object Dependencies {
   val dependencies = Seq(
     "com.typesafe" % "config" % "1.4.2",
     // NOTE: doobie 1.0.0-RC2 dependes on cats-effect 3.3.4, but cats-effect keep fully compatibility with 3.x
-    "org.typelevel" %% "cats-effect" % "3.4.8",
+    "org.typelevel" %% "cats-effect" % "3.4.9",
     "org.http4s" %% "http4s-dsl" % http4sVersion,
     "org.http4s" %% "http4s-ember-server" % http4sVersion,
     "org.http4s" %% "http4s-ember-client" % http4sVersion % Test,
@@ -22,8 +22,8 @@ object Dependencies {
     "org.mariadb.jdbc" % "mariadb-java-client" % "3.1.3",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "org.wvlet.airframe" %% "airframe-ulid" % "23.3.4",
-    "com.github.ben-manes.caffeine" % "caffeine" % "3.1.5",
+    "org.wvlet.airframe" %% "airframe-ulid" % "23.4.2",
+    "com.github.ben-manes.caffeine" % "caffeine" % "3.1.6",
     "org.flywaydb" % "flyway-core" % flywayVersion,
     "org.flywaydb" % "flyway-mysql" % flywayVersion,
     "ch.qos.logback" % "logback-classic" % "1.4.6",
@@ -32,6 +32,6 @@ object Dependencies {
     "org.springframework.security" % "spring-security-core" % "6.0.2",
     "org.slf4j" % "slf4j-api" % "2.0.7",
     "org.scalatest" %% "scalatest" % "3.2.15" % "test",
-    "org.mockito" % "mockito-core" % "5.2.0" % "test"
+    "org.mockito" % "mockito-core" % "5.3.0" % "test"
   )
 }
