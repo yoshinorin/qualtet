396619a3835784f577c8230dc45c8cad2adc1576
yoshinorin <yoshinorin.net@outlook.com>
2023-05-12 01:02:16 +0900

    chore(series): rename junction table for `contents` and `series`

--- src/main/resources/db/migration/V2.5.0__series.sql
+++ src/main/resources/db/migration/V2.5.0__series.sql
@@ -5,11 +5,10 @@ CREATE TABLE IF NOT EXISTS series (
     description VARCHAR(1024) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS series_contents_belonging (
+CREATE TABLE IF NOT EXISTS contents_serializing (
     series_id CHAR(26) CHARACTER SET ascii NOT NULL,
     content_id CHAR(26) CHARACTER SET ascii NOT NULL,
     PRIMARY KEY (series_id, content_id),
-    FOREIGN KEY fk_series_from_series_contents_belonging(series_id) REFERENCES series(id),
-    FOREIGN KEY fk_content_from_series_contents_belonging(content_id) REFERENCES contents(id)
+    FOREIGN KEY fk_series_from_contents_serializing(series_id) REFERENCES series(id),
+    FOREIGN KEY fk_content_from_contents_serializing(content_id) REFERENCES contents(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
