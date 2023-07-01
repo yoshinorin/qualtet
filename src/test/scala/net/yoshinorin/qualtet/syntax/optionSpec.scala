410edfcced436ed0501dda0608fdbf38f8684976
yoshinorin <yoshinorin.net@outlook.com>
2023-07-01 22:38:56 +0900

    test(syntax): add `orThrow` test case

--- src/test/scala/net/yoshinorin/qualtet/syntax/optionSpec.scala
+++ src/test/scala/net/yoshinorin/qualtet/syntax/optionSpec.scala
@@ -1,6 +1,5 @@
 package net.yoshinorin.qualtet.syntax
 
-import cats.effect.IO
 import org.scalatest.wordspec.AnyWordSpec
 import net.yoshinorin.qualtet.message.Fail
 import net.yoshinorin.qualtet.message.Fail.UnprocessableEntity
@@ -10,6 +9,21 @@ import cats.effect.unsafe.implicits.global
 class OptionSpec extends AnyWordSpec {
 
   "option syntax" should {
+
+    "orThrow" should {
+
+      "not be throw if not None" in {
+        assert(Some("some values").orThrow(UnprocessableEntity("unprocessable!!")) === "some values")
+      }
+
+      "be thrown if None" in {
+        assertThrows[UnprocessableEntity] {
+          assert(None.orThrow(UnprocessableEntity("unprocessable!!")))
+        }
+      }
+
+    }
+
     "asEither" should {
 
       "be return Right if not None" in {
