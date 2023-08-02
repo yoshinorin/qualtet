a2f8464b2ffd56bf63c2f34d99f17a96a408f4d5
yoshinorin <yoshinorin.net@outlook.com>
2023-08-03 02:50:15 +0900

    test(http/header): add test case for additional headers

--- src/test/scala/net/yoshinorin/qualtet/http/ResponseHeadersSpec.scala
+++ src/test/scala/net/yoshinorin/qualtet/http/ResponseHeadersSpec.scala
@@ -1,4 +1,3 @@
-/*
 package net.yoshinorin.qualtet.http.routes
 
 import cats.effect.IO
@@ -7,6 +6,7 @@ import org.http4s.*
 import org.http4s.dsl.io.*
 import org.http4s.implicits.*
 import org.typelevel.ci._
+import net.yoshinorin.qualtet.HttpAppBuilder
 import net.yoshinorin.qualtet.fixture.Fixture.router
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -15,8 +15,8 @@ import cats.effect.unsafe.implicits.global
 // testOnly net.yoshinorin.qualtet.http.routes.ResponseHeadersSpec
 class ResponseHeadersSpec extends AnyWordSpec {
 
-  // TODO: build from `BootStrap.buildHttpApp`
-  val client: Client[IO] = Client.fromHttpApp(router.routes.orNotFound)
+  val httpApp = new HttpAppBuilder(router.routes.orNotFound).build
+  val client: Client[IO] = Client.fromHttpApp(httpApp)
 
   "Response" should {
 
@@ -25,9 +25,38 @@ class ResponseHeadersSpec extends AnyWordSpec {
         .run(Request(method = Method.GET, uri = uri"/"))
         .use { response =>
           IO {
-            println(response.headers)
-            assert(response.headers.get(ci"X-Request-Id").isInstanceOf[String])
-            assert(response.headers.get(ci"X-Response-Time").isInstanceOf[Int])
+            val reqId = response.headers.get(ci"X-Request-Id")
+            assert(reqId.isDefined)
+            val resTime = response.headers.get(ci"X-Response-Time")
+            assert(resTime.isDefined)
+          }
+        }
+        .unsafeRunSync()
+    }
+
+    "be contains x-request-id header and its value is same with request value (UUID4)" in {
+      val uuid4String = "76de4439-1521-4bf4-86bf-ccd38afc416a"
+      client
+        .run(Request(method = Method.GET, uri = uri"/", headers = Headers(Header.Raw(ci"X-Request-Id", uuid4String))))
+        .use { response =>
+          IO {
+            val reqId = response.headers.get(ci"X-Request-Id")
+            assert(reqId.isDefined)
+            assert(reqId.head.head.value === uuid4String)
+          }
+        }
+        .unsafeRunSync()
+    }
+
+    "be contains x-request-id header and its value is same with request value (NOT a UUID)" in {
+      val notUuidString = "not uuid string"
+      client
+        .run(Request(method = Method.GET, uri = uri"/", headers = Headers(Header.Raw(ci"X-Request-Id", notUuidString))))
+        .use { response =>
+          IO {
+            val reqId = response.headers.get(ci"X-Request-Id")
+            assert(reqId.isDefined)
+            assert(reqId.head.head.value === notUuidString)
           }
         }
         .unsafeRunSync()
@@ -35,4 +64,3 @@ class ResponseHeadersSpec extends AnyWordSpec {
   }
 
 }
-*/
