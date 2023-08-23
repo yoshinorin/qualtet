a4a9e9c5a9f84cc6dbba7e1e68afa00c730f2b81
yoshinorin <yoshinorin.net@outlook.com>
2023-08-23 21:27:12 +0900

    feat(http/system): return more field when `GET` request to `metadata` endpoint

--- src/main/scala/net/yoshinorin/qualtet/ApplicationInfo.scala
+++ src/main/scala/net/yoshinorin/qualtet/ApplicationInfo.scala
@@ -3,20 +3,33 @@ package net.yoshinorin.qualtet
 import net.yoshinorin.qualtet.buildinfo.BuildInfo
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 import com.github.plokhotnyuk.jsoniter_scala.core.*
+import net.yoshinorin.qualtet.buildinfo.BuildInfo.repository
 
 lazy val jvmVendor = System.getProperty("java.vendor")
 lazy val runtimeVersion = System.getProperty("java.version")
 lazy val commitHash = BuildInfo.commitHash.substring(0, 7)
+lazy val runtime = Runtime()
+lazy val build = Build()
+
+private final case class Runtime(
+  name: String = "Java",
+  vendor: String = jvmVendor,
+  version: String = runtimeVersion
+)
+
+private final case class Build(
+  commit: String = commitHash,
+  url: String = s"${BuildInfo.repository}/commit/${commitHash}",
+  scalaVersion: String = BuildInfo.scalaVersion,
+  sbtVersion: String = BuildInfo.sbtVersion
+)
 
 final case class ApplicationInfo(
   name: String = BuildInfo.name,
   version: String = BuildInfo.version,
-  scalaVersion: String = BuildInfo.scalaVersion,
-  sbtVersion: String = BuildInfo.sbtVersion,
-  commitHash: String = commitHash,
-  runtime: String = "Java",
-  jvmVendor: String = jvmVendor,
-  runtimeVersion: String = runtimeVersion
+  repository: String = BuildInfo.repository,
+  runtime: Runtime = runtime,
+  build: Build = build
 )
 
 object ApplicationInfo {
