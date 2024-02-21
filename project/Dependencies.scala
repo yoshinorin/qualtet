e7e7ea5d7e017ce250caef2041cd3407eb1d0ae7
yoshinorin <yoshinorin.net@outlook.com>
2024-02-22 00:03:56 +0900

    chore(deps): bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt.*
 
 object Dependencies {
 
-  val jsoniterVersion = "2.28.1"
+  val jsoniterVersion = "2.28.2"
   val doobieVersion = "1.0.0-RC5"
   val jwtScalaVersion = "10.0.0"
-  val flywayVersion = "10.7.1"
+  val flywayVersion = "10.8.1"
   val http4sVersion = "1.0.0-M40"
   val log4catsVersion = "2.6.0"
 
@@ -22,19 +22,19 @@ object Dependencies {
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % jsoniterVersion,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "compile-internal",
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "test-internal",
-    "org.mariadb.jdbc" % "mariadb-java-client" % "3.3.2",
+    "org.mariadb.jdbc" % "mariadb-java-client" % "3.3.3",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "org.wvlet.airframe" %% "airframe-ulid" % "24.1.2",
+    "org.wvlet.airframe" %% "airframe-ulid" % "24.2.0",
     "com.github.ben-manes.caffeine" % "caffeine" % "3.1.8",
     "org.flywaydb" % "flyway-core" % flywayVersion,
     "org.flywaydb" % "flyway-mysql" % flywayVersion,
-    "ch.qos.logback" % "logback-classic" % "1.4.14",
+    "ch.qos.logback" % "logback-classic" % "1.5.0",
     "net.logstash.logback" % "logstash-logback-encoder" % "7.4",
     "org.codehaus.janino" % "janino" % "3.1.12",
-    "org.springframework.security" % "spring-security-core" % "6.2.1",
+    "org.springframework.security" % "spring-security-core" % "6.2.2",
     "org.slf4j" % "slf4j-api" % "2.0.12",
-    "org.scalatest" %% "scalatest" % "3.2.17" % "test",
+    "org.scalatest" %% "scalatest" % "3.2.18" % "test",
     "org.mockito" % "mockito-core" % "5.10.0" % "test"
   )
 }
