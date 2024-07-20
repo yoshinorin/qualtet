b74af357f706293e5d299cc5c88673c1430aa47d
yoshinorin <yoshinorin.net@outlook.com>
2024-07-20 23:16:32 +0900

    test(task): enable comment-outed assertion

--- src/test/scala/net/yoshinorin/qualtet/tasks/CreateAuthorSpec.scala
+++ src/test/scala/net/yoshinorin/qualtet/tasks/CreateAuthorSpec.scala
@@ -13,16 +13,11 @@ class CreateAuthorSpec extends AnyWordSpec {
     "create author" in {
       CreateAuthor.run(List(author.name.value, author.displayName.value, "pass")).unsafeRunSync()
 
-      // TODO: Need assertion. Sometimes return `None` after change 9817e9fc7a57cb1e8eac1c168a715f9486ed0dc7
-      // val a = authorService.findByName(AuthorName(author.name.value)).unsafeRunSync()
-      // assert(a.get.name.value === author.name.value)
+      val a = authorService.findByName(AuthorName(author.name.value)).unsafeRunSync()
+      assert(a.get.name.value === author.name.value)
 
       // NOTE: just for create test data
       CreateAuthor.run(List(author2.name.value, author2.displayName.value, "pass")).unsafeRunSync()
-      // NOTE: avoid test failure. This is a just test data no need assert.
-      // assert(a2.get.name.value === author2.name.value)
-
-      // CreateAuthor.main(Array("notexistsuser", "NA", "pass"))
     }
 
     "not be create author" in {
