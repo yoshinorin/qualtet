3d912e729b565adec5048d499c0cd5af4ddcc868
yoshinorin <yoshinorin.net@outlook.com>
2025-04-06 06:48:25 +0900

    refactor(auth): use `log4cats` for logging

--- src/main/scala/net/yoshinorin/qualtet/http/AuthProvider.scala
+++ src/main/scala/net/yoshinorin/qualtet/http/AuthProvider.scala
@@ -6,39 +6,46 @@ import cats.data.{Kleisli, OptionT}
 import org.http4s.server.AuthMiddleware
 import org.http4s.{AuthedRoutes, Request, Response, Status}
 import org.http4s.headers.Authorization
-import org.slf4j.LoggerFactory
 import net.yoshinorin.qualtet.domains.authors.AuthorResponseModel
 import net.yoshinorin.qualtet.auth.AuthService
 import net.yoshinorin.qualtet.domains.errors.{AuthorNotFound, DomainError, Unauthorized}
 import net.yoshinorin.qualtet.syntax.*
+import org.typelevel.log4cats.{LoggerFactory => Log4CatsLoggerFactory, SelfAwareStructuredLogger}
 
 class AuthProvider[F[_]: Monad](
   authService: AuthService[F]
-) {
-  private val logger = LoggerFactory.getLogger(this.getClass)
+)(using loggerFactory: Log4CatsLoggerFactory[IO]) {
+
+  private val logger: SelfAwareStructuredLogger[IO] = loggerFactory.getLoggerFromClass(this.getClass)
 
   // TODO: cleanup messy code
   private def authUser: Kleisli[IO, Request[IO], Either[DomainError, (AuthorResponseModel, String)]] =
     Kleisli({ request =>
-      request.headers.get[Authorization].asEither[DomainError](Unauthorized("Authorization header is none")) match {
-        case Left(f: DomainError) => IO(Left(f))
-        case Right(auth: Authorization) =>
-          val renderString = auth.credentials.renderString.replace("Bearer ", "").replace("bearer ", "")
-          for {
+      val authorizationHeader = request.headers.get[Authorization].asEither[DomainError](Unauthorized("Authorization header is none")) match {
+        case Right(auth: Authorization) => Right(auth)
+        case Left(f: DomainError) => Left(f)
+      }
+
+      authorizationHeader match {
+        case Left(e) => IO.pure(Left(e))
+        case Right(a) => {
+          (for {
+            renderString <- IO(a.credentials.renderString.replace("Bearer ", "").replace("bearer ", ""))
             maybeAuthor <- authService.findAuthorFromJwtString(renderString)
             author <- IO(maybeAuthor.asEither[DomainError](AuthorNotFound(detail = "author not found")))
             payload <- request.as[String]
           } yield {
             author match {
               case Left(fail) => {
-                logger.error(fail.getMessage)
-                Left(fail)
+                logger.error(fail.getMessage) *>
+                  IO.pure(Left(fail))
               }
               case Right(value) =>
-                logger.info(s"Authorization succeeded: ${value}")
-                Right((value, payload))
+                logger.info(s"Authorization succeeded: ${value}") *>
+                  IO.pure(Right((value, payload)))
             }
-          }
+          }).flatMap(identity)
+        }
       }
     })
 
