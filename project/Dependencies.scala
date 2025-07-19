5ae4d245de9c5c80e79d10329070457af63d4a9a
yoshinorin <yoshinorin.net@outlook.com>
2025-07-20 02:36:48 +0900

    chore(deps): update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object Dependencies {
 
-  val jsoniterVersion = "2.35.1"
-  val doobieVersion = "1.0.0-RC9"
-  val jwtScalaVersion = "10.0.4"
-  val flywayVersion = "11.7.2"
+  val jsoniterVersion = "2.36.7"
+  val doobieVersion = "1.0.0-RC10"
+  val jwtScalaVersion = "11.0.2"
+  val flywayVersion = "11.10.3"
   val http4sVersion = "1.0.0-M44"
-  val log4catsVersion = "2.7.0"
+  val log4catsVersion = "2.7.1"
 
   val dependencies = Seq(
-    "com.typesafe" % "config" % "1.4.3",
+    "com.typesafe" % "config" % "1.4.4",
     // NOTE: doobie 1.0.0-RC4 and http4s 1.0.0-M40 dependes on cats-effect 3.5.1
     "org.typelevel" %% "cats-effect" % "3.6.1",
     "org.typelevel" %% "log4cats-core" % log4catsVersion,
@@ -22,20 +22,20 @@ object Dependencies {
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % jsoniterVersion,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "compile-internal",
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "test-internal",
-    "org.mariadb.jdbc" % "mariadb-java-client" % "3.5.3",
+    "org.mariadb.jdbc" % "mariadb-java-client" % "3.5.4",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-hikari" % doobieVersion,
     // "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "org.wvlet.airframe" %% "airframe-ulid" % "2025.1.10",
-    "com.github.ben-manes.caffeine" % "caffeine" % "3.2.0",
+    "org.wvlet.airframe" %% "airframe-ulid" % "2025.1.14",
+    "com.github.ben-manes.caffeine" % "caffeine" % "3.2.2",
     "org.flywaydb" % "flyway-core" % flywayVersion,
     "org.flywaydb" % "flyway-mysql" % flywayVersion,
     "ch.qos.logback" % "logback-classic" % "1.5.18",
     "net.logstash.logback" % "logstash-logback-encoder" % "8.1",
     "org.codehaus.janino" % "janino" % "3.1.12",
-    "org.springframework.security" % "spring-security-core" % "6.4.4",
+    "org.springframework.security" % "spring-security-core" % "6.5.1",
     "org.slf4j" % "slf4j-api" % "2.0.17",
     "org.scalatest" %% "scalatest" % "3.2.19" % "test",
-    "org.mockito" % "mockito-core" % "5.17.0" % "test"
+    "org.mockito" % "mockito-core" % "5.18.0" % "test"
   )
 }
