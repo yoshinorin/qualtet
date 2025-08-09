2e072812b765fd2775503c14998fb7ba3ed46cec
yoshinorin <yoshinorin.net@outlook.com>
2025-08-09 15:30:57 +0900

    chore(deps): bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,41 +2,41 @@ import sbt.*
 
 object Dependencies {
 
-  val jsoniterVersion = "2.36.7"
+  val jsoniterVersion = "2.37.1"
   val doobieVersion = "1.0.0-RC10"
   val jwtScalaVersion = "11.0.2"
-  val flywayVersion = "11.10.3"
+  val flywayVersion = "11.11.0"
   val http4sVersion = "1.0.0-M44"
   val log4catsVersion = "2.7.1"
 
   val dependencies = Seq(
     "com.typesafe" % "config" % "1.4.4",
     // NOTE: doobie 1.0.0-RC4 and http4s 1.0.0-M40 dependes on cats-effect 3.5.1
-    "org.typelevel" %% "cats-effect" % "3.6.1",
+    "org.typelevel" %% "cats-effect" % "3.6.3",
     "org.typelevel" %% "log4cats-core" % log4catsVersion,
     "org.typelevel" %% "log4cats-slf4j" % log4catsVersion,
     "org.http4s" %% "http4s-dsl" % http4sVersion,
     "org.http4s" %% "http4s-ember-server" % http4sVersion,
     "org.http4s" %% "http4s-ember-client" % http4sVersion % Test,
     "org.typelevel" %% "otel4s-oteljava" % "0.13.1",
-    "io.opentelemetry" % "opentelemetry-exporter-otlp" % "1.52.0",
-    "io.opentelemetry" % "opentelemetry-sdk-extension-autoconfigure" % "1.52.0",
+    "io.opentelemetry" % "opentelemetry-exporter-otlp" % "1.53.0",
+    "io.opentelemetry" % "opentelemetry-sdk-extension-autoconfigure" % "1.53.0",
     "com.github.jwt-scala" %% "jwt-core" % jwtScalaVersion,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % jsoniterVersion,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "compile-internal",
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion % "test-internal",
-    "org.mariadb.jdbc" % "mariadb-java-client" % "3.5.4",
+    "org.mariadb.jdbc" % "mariadb-java-client" % "3.5.5",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-hikari" % doobieVersion,
     // "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "org.wvlet.airframe" %% "airframe-ulid" % "2025.1.14",
+    "org.wvlet.airframe" %% "airframe-ulid" % "2025.1.15",
     "com.github.ben-manes.caffeine" % "caffeine" % "3.2.2",
     "org.flywaydb" % "flyway-core" % flywayVersion,
     "org.flywaydb" % "flyway-mysql" % flywayVersion,
     "ch.qos.logback" % "logback-classic" % "1.5.18",
     "net.logstash.logback" % "logstash-logback-encoder" % "8.1",
     "org.codehaus.janino" % "janino" % "3.1.12",
-    "org.springframework.security" % "spring-security-core" % "6.5.1",
+    "org.springframework.security" % "spring-security-core" % "6.5.2",
     "org.slf4j" % "slf4j-api" % "2.0.17",
     "org.scalatest" %% "scalatest" % "3.2.19" % "test",
     "org.mockito" % "mockito-core" % "5.18.0" % "test",
